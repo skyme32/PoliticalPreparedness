8f9a714beca3541944fc5e98b3e13582a2d40132
skyme32 <marcosmejiasg@gmail.com>
2021-03-22 13:48:21 +0100

    Generate MotionScene and Viewmodel with service api

--- app/src/main/java/com/example/android/politicalpreparedness/representative/RepresentativeViewModel.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/representative/RepresentativeViewModel.kt
@@ -1,12 +1,25 @@
 package com.example.android.politicalpreparedness.representative
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.android.politicalpreparedness.network.CivicsApi
+import com.example.android.politicalpreparedness.network.models.Address
+import com.example.android.politicalpreparedness.representative.model.Representative
+import kotlinx.coroutines.launch
 
 class RepresentativeViewModel: ViewModel() {
 
-    //TODO: Establish live data for representatives and address
+    // Establish live data for representatives and address
+    private val _representatives = MutableLiveData<List<Representative>>()
+    val representatives: LiveData<List<Representative>>
+        get() = _representatives
 
-    //TODO: Create function to fetch representatives from API from a provided address
+    private val _address = MutableLiveData<Address>()
+    val address: LiveData<Address>
+        get() = _address
+
 
     /**
      *  The following code will prove helpful in constructing a representative from the API. This code combines the two nodes of the RepresentativeResponse into a single official :
@@ -16,8 +29,19 @@ class RepresentativeViewModel: ViewModel() {
 
     Note: getRepresentatives in the above code represents the method used to fetch data from the API
     Note: _representatives in the above code represents the established mutable live data housing representatives
-
      */
+    // Create function to fetch representatives from API from a provided address
+    fun getRepresentatives() {
+        viewModelScope.launch {
+            val (offices, officials) = CivicsApi.retrofitService.getRepresentatives(_address.value!!.toFormattedString())
+            _representatives.postValue(offices.flatMap { office ->
+                office.getRepresentatives(officials) }
+            )
+        }
+    }
+
+
+
 
     //TODO: Create function get address from geo location
 
